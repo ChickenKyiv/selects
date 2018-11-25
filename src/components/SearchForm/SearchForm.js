c2b16e48dc60dc19aaef8283f5ae0a26fb94095b
Arthur Tkachenko <arthur.tkachenko.netweight@gmail.com>
2018-11-26 01:06:15 +0200

    update? it works

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -7,11 +7,10 @@ import {
   Select
 } from 'antd';
 
-// import Select from 'react-select';
-// import Animated from 'react-select/lib/animated';
+
 import _ from 'lodash';
 import SelectContainer from './SelectContainer';
-// import _ from 'underscore';
+
 
 
 //@TODO test it later with IDS instead of text at values
@@ -25,15 +24,10 @@ const options = [
   { value: 'vanilla double',    label: 'Vanilla Double' }
 ];
 
-// const options = [
-//   { value: 'chocolate',  label: 'Chocolate',  isDisabled: true },
-//   { value: 'strawberry', label: 'Strawberry', isDisabled: false },
-//   { value: 'vanilla',    label: 'Vanilla',    isDisabled: false },
-//   { value: 'vanilla-ice',    label: 'Vanilla Ice',    isDisabled: false },
-//   { value: 'vanilla latte',    label: 'Vanilla Latte',    isDisabled: false },
-//   { value: 'vanilla Chino',    label: 'Vanilla Chino',    isDisabled: false },
-//   { value: 'vanilla double',    label: 'Vanilla Double',    isDisabled: false }
-// ];
+
+
+// @TODO this component can do better just by calling a different db queries for each of select.
+// so it's not necessary to manipulate it in this way.
 
 class SearchForm extends Component {
 
@@ -57,81 +51,51 @@ class SearchForm extends Component {
     // from parent component into child component
   }
 
+
   handleChange1 = (selected) => {
 
-    // _.map((options),(item) => {
-    //
-    //   // console.log(item)
-    //   item.isDisabled = true;
-    // });
     // really bored, and making names badly. @TODO change it, ple A se
     var clone_of_state_2 = this.state.options2;
 
-    // _.map(selected, (item) => {
-    //   // console.log(item);
-    //   var option = _.find(clone_of_state_2, { 'value': item });
-    //   console.log(option);
-    //
-    //
-    // })
-    //   console.log(options)
 
-      // console.log(clone_of_state_2['value'][selected[0]]);
       var kle = _.map(clone_of_state_2, (item) => {
 
-        console.log( _.find(selected, item.value) )  
-        // _.mapValues(item, val => {
-        //   console.log(val);
-        // })
+        console.log( selected.includes(item.value) )
+        if( selected.includes(item.value) ){
+          item.isDisabled = true;
+        }
       })
 
 
-
-    // var values = [];
-    // _.map(options,(item) => {
-    //   values.push({ 'value': item.value, 'label': item.label });
-    // })
-    // console.log(values)
-    //
-
     this.setState({ selectedOption1: selected });
-    // console.log(`Option selected:`, options);
+
   }
 
-  handleChange2 = (options) => {
+  handleChange2 = (selected) => {
 
+    var clone_of_state_1 = this.state.options1;
+    var kle = _.map(clone_of_state_1, (item) => {
+
+      console.log( selected.includes(item.value) )
+      if( selected.includes(item.value) ){
+        item.isDisabled = true;
+      }
+
+    })
     // this.setState({ selectedOption2: values });
 
-    this.setState({ selectedOption2: options });
-    // console.log(`Option selected:`, options);
+    this.setState({ selectedOption2: selected });
   }
 
-  componentDidMount() {
-   console.log('mount');
- }
+ //  componentDidMount() {
+ //   console.log('mount');
+ // }
 
- proceedDisabledOptions() {
-
-   const { selectedOption1, selectedOption2, options1 } = this.state;
-
-
-   // console.log(selectedOption1)
-   // console.log(selectedOption2)
-
-
-   _.map((options1),(item) => {
-
-     // console.log(item)
-     item.isDisabled = true;
-   });
-
-   console.log(options1);
- }
 
  componentDidUpdate(prevProps, prevState) {
-   console.log('update');
+   // console.log('update');
 
-   // this.proceedDisabledOptions() ;
+
 
 
  }
