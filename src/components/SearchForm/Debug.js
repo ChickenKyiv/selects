cc104bd8880e84ac54274a244528e819fff51ce0
Arthur Tkachenko <atherdon@gmail.com>
2018-12-13 08:33:57 +0200

    +

--- src/components/SearchForm/Debug.js
+++ src/components/SearchForm/Debug.js
@@ -2,26 +2,6 @@ import React, { Component, Fragment } from 'react';
 
 class Debug extends Component {
 
-  constructor(props) {
-    super(props);
-
-    // this.state = {
-    //   // name       : ( (props.flag) ? 'allowed' : 'excluded' ) + props.placeholder,
-    //   name       : 'the_name',
-    //   placeholder: props.placeholder
-    // }
-    // // console.log(props.placeholder)
-    // this.handleChange  = this.handleChange.bind(this);
-
-
-  }
-
-  // handleChange(items) {
-  //   // console.log(items);
-  //   this.props.onChange(items)
-  // }
-
-
   render() {
 
       // const options = this.props.options;
